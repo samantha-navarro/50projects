02dbe2a326a70ba641a76be4e0853edd78448e5c
Samantha Navarro <samantha.m.navarro@gmail.com>
2023-02-13 19:27:26 -0800

    html and css done for project 5

---
+++ 5. blurry-image-loading/script.js
@@ -0,0 +1,11 @@
+// getting DOM ELEMENTS
+
+const loadingText = document.querySelector('.loading-text');
+const backgroundImage = document.querySelector('.background-image');
+
+// start percentage at 0
+
+let load = 0;
+
+// FUNCTIONS
+
