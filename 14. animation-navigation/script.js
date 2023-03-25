22694dcf91076ffe5ba660c79563b81969360ac7
Samantha Navarro <samantha.m.navarro@gmail.com>
2023-03-25 16:14:30 -0700

    added project starter

--- 14. animation-navigation/script.js
+++ 14. animation-navigation/script.js
@@ -1,5 +1,7 @@
 const toggle = document.getElementById('toggle');
+// button to toggle
 const nav = document.getElementById('nav');
+// nav container to toggle
 
 toggle.addEventListener('click', () => {
   nav.classList.toggle('active');
